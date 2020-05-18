01a7f2ab051a3d403480041a33c6deba45202813
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2020-05-18 20:20:50 +0200

    Deserialize timestamps

--- src/standardfile/mod.rs
+++ src/standardfile/mod.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use chrono::{DateTime, Utc};
 use serde::Deserialize;
 
 pub mod crypto;
@@ -17,6 +18,8 @@ pub struct Item {
     pub content: String,
     pub content_type: String,
     pub enc_item_key: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
 }
 
 #[derive(Deserialize, Debug)]
