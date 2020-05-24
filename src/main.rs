b410ad0fcf448ac9df175497c3a667b546cc575d
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2020-05-24 20:10:35 +0200

    Use secret-service to fetch password

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #[macro_use]
 extern crate glib;
+extern crate secret_service;
 
 mod config;
 mod standardfile;
 mod ui;
 
+use anyhow::{Context, Result};
 use gio::{resources_register, Resource};
 use glib::Bytes;
-use anyhow::{Context, Result};
+use secret_service::{EncryptionType, SecretService};
 use standardfile::Root;
 use ui::application::Application;
 
@@ -20,13 +22,30 @@ fn init_resources() -> Result<()> {
     Ok(())
 }
 
+fn get_password(email: &String) -> Result<String> {
+    let service = SecretService::new(EncryptionType::Dh).unwrap();
+
+    let items = service
+        .search_items(vec![
+            ("service", "standardnotes"),
+            ("email", email),
+            ("server", "https://app.standardnotes.org"),
+        ])
+        .unwrap();
+
+    let item = items.get(0).unwrap();
+    let pass = item.get_secret().unwrap();
+
+    Ok(String::from_utf8(pass)?)
+}
+
 fn main() -> Result<()> {
     let filename = "test.json";
     let contents = std::fs::read_to_string(filename)
         .with_context(|| format!("Could not open {}.", filename))?;
 
     let root: Root = serde_json::from_str(&contents)?;
-    let pass = std::env::var("SF_PASS").unwrap();
+    let pass = get_password(&std::env::var("SF_EMAIL")?)?;
 
     for item in root.notes(&pass)? {
         println!("{}", item.item.updated_at);
