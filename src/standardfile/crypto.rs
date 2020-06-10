697d82a06427355ed24fe675beed86457b3d805d
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2020-06-10 21:45:00 +0200

    Add encryption and test

--- src/standardfile/crypto.rs
+++ src/standardfile/crypto.rs
@@ -1,11 +1,14 @@
-use super::{AuthParams, Item};
+use super::{AuthParams, Item, Note};
+use crate::models;
 use aes::Aes256;
 use anyhow::Result;
 use block_modes::block_padding::Pkcs7;
 use block_modes::{BlockMode, Cbc};
 use data_encoding::{BASE64, HEXLOWER};
+use rand::prelude::*;
 use ring::{digest, hmac};
 use std::str;
+use uuid::Uuid;
 
 pub type Key = [u8; 768 / 8 / 3];
 
@@ -14,8 +17,10 @@ pub struct Crypto {
     ak: Key,
 }
 
-pub fn decrypt<S: AsRef<str>>(s: S, ek: &Key, ak: &Key, check_uuid: S) -> Result<String> {
-    let s: Vec<&str> = s.as_ref().split(':').collect();
+type Aes256Cbc = Cbc<Aes256, Pkcs7>;
+
+fn decrypt(s: &str, ek: &Key, ak: &Key, check_uuid: &str) -> Result<String> {
+    let s: Vec<&str> = s.split(':').collect();
     let version = s[0];
     let auth_hash = s[1];
     let uuid = s[2];
@@ -23,19 +28,44 @@ pub fn decrypt<S: AsRef<str>>(s: S, ek: &Key, ak: &Key, check_uuid: S) -> Result
     let ciphertext = s[4];
 
     assert!(version == "003");
-    assert!(check_uuid.as_ref() == uuid);
+    assert!(check_uuid == uuid);
 
     let to_auth = std::format!("003:{}:{}:{}", uuid, iv, ciphertext);
     let auth_hash_bytes = HEXLOWER.decode(&auth_hash.as_bytes())?;
     let key = hmac::Key::new(hmac::HMAC_SHA256, ak);
     hmac::verify(&key, to_auth.as_bytes(), &auth_hash_bytes).expect("foo");
 
-    type Aes256Cbc = Cbc<Aes256, Pkcs7>;
     let iv_bytes = HEXLOWER.decode(iv.as_bytes())?;
     let cipher = Aes256Cbc::new_var(ek, &iv_bytes)?;
     let ciphertext_bytes = BASE64.decode(ciphertext.as_bytes())?;
     let decrypted = cipher.decrypt_vec(ciphertext_bytes.as_ref())?;
     Ok(str::from_utf8(decrypted.as_ref())?.to_string())
+}
+
+fn encrypt(s: &str, ek: &Key, ak: &Key, uuid: &Uuid) -> Result<String> {
+    // FIXME: use a different seed, obviously ...
+    let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(10);
+    let mut iv_bytes = [0u8; 16];
+    rng.fill_bytes(&mut iv_bytes);
+
+    let uuid_encoded = uuid.to_hyphenated_ref();
+    let cipher = Aes256Cbc::new_var(ek, &iv_bytes)?;
+    let encrypted = cipher.encrypt_vec(s.as_ref());
+    let encrypted_encoded = BASE64.encode(encrypted.as_slice());
+    let iv_encoded = HEXLOWER.encode(iv_bytes.as_ref());
+    let to_auth = std::format!("003:{}:{}:{}", uuid_encoded, iv_encoded, encrypted_encoded);
+    let key = hmac::Key::new(hmac::HMAC_SHA256, ak.as_ref());
+    let to_auth_bytes = to_auth.as_bytes();
+    let auth_hash_bytes = hmac::sign(&key, to_auth_bytes);
+    let auth_hash = HEXLOWER.encode(auth_hash_bytes.as_ref());
+
+    Ok(std::format!(
+        "003:{}:{}:{}:{}",
+        auth_hash,
+        uuid_encoded,
+        iv_encoded,
+        encrypted_encoded
+    ))
 }
 
 impl Crypto {
@@ -60,10 +90,7 @@ impl Crypto {
         mk.clone_from_slice(&hashed[32..64]);
         ak.clone_from_slice(&hashed[64..]);
 
-        Ok(Crypto {
-            mk: mk,
-            ak: ak,
-        })
+        Ok(Crypto { mk: mk, ak: ak })
     }
 
     pub fn decrypt(&self, item: &Item) -> Result<String> {
@@ -79,5 +106,73 @@ impl Crypto {
             .expect("foo");
 
         Ok(decrypt(&item.content, &item_ek, &item_ak, &item.uuid)?)
+    }
+
+    pub fn encrypt(&self, note: &models::Note, uuid: &Uuid) -> Result<Item> {
+        let json_note = Note {
+            title: Some(note.title.clone()),
+            text: note.text.clone(),
+        };
+
+        // FIXME: use a different seed, obviously ...
+        let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(10);
+        let mut item_key = [0u8; 64];
+        rng.fill_bytes(&mut item_key);
+
+        let mut item_ek: Key = [0; 32];
+        let mut item_ak: Key = [0; 32];
+
+        item_ek.clone_from_slice(&item_key[..32]);
+        item_ak.clone_from_slice(&item_key[32..]);
+
+        let to_encrypt = serde_json::to_string(&json_note)?;
+
+        let mut iv_bytes = [0u8; 16];
+        rng.fill_bytes(&mut iv_bytes);
+
+        let item_key_encoded = HEXLOWER.encode(item_key.as_ref());
+
+        Ok(Item {
+            uuid: uuid.to_hyphenated().to_string(),
+            content: encrypt(to_encrypt.as_ref(), &item_ek, &item_ak, &uuid)?,
+            content_type: "Note".to_owned(),
+            enc_item_key: encrypt(item_key_encoded.as_ref(), &self.mk, &self.ak, &uuid)?,
+            created_at: note.created_at,
+            updated_at: note.updated_at,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::Utc;
+
+    #[test]
+    fn test_encrypt_decrypt() {
+        let now = Utc::now();
+
+        let note = models::Note {
+            title: "Title".to_owned(),
+            text: "Text".to_owned(),
+            created_at: now,
+            updated_at: now,
+        };
+
+        let auth_params = AuthParams {
+            identifier: "foo@bar.com".to_owned(),
+            pw_cost: 110000,
+            pw_nonce: "3f8ea1ffd8067c1550ca3ad78de71c9b6e68b5cb540e370c12065eca15d9a049".to_owned(),
+            version: "003".to_owned(),
+        };
+
+        let crypto = Crypto::new(&auth_params, "foobar").unwrap();
+        let uuid = Uuid::new_v4();
+        let encrypted = crypto.encrypt(&note, &uuid).unwrap();
+        let decrypted = crypto.decrypt(&encrypted).unwrap();
+        let item = serde_json::from_str::<Note>(&decrypted).unwrap();
+
+        assert_eq!(item.title.unwrap(), note.title);
+        assert_eq!(item.text, note.text);
     }
 }
