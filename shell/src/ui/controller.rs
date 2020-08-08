966195b9db5321e6589522b4550dd0814c5d57a1
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2020-08-08 22:40:29 +0200

    Use HashMap to map rows to items

--- shell/src/ui/controller.rs
+++ shell/src/ui/controller.rs
@@ -2,18 +2,17 @@ use chrono::{DateTime, Utc};
 use gio::prelude::*;
 use gtk::prelude::*;
 use standardfile::Note;
-use std::{cell::RefCell, cmp, cmp::{Ord, Ordering}, rc::Rc};
+use std::{cell::RefCell, cmp, cmp::{Ord, Ordering}, collections::HashMap, rc::Rc};
 use uuid::Uuid;
 
 struct Item {
     uuid: Uuid,
-    row: gtk::ListBoxRow,
     label: gtk::Label,
     last_updated: DateTime<Utc>,
 }
 
 pub struct Controller {
-    items: Rc<RefCell<Vec<Item>>>,
+    items: Rc<RefCell<HashMap<gtk::ListBoxRow, Item>>>,
     list_box: gtk::ListBox,
     title_entry: gtk::Entry,
     note_stack: gtk::Stack,
@@ -45,7 +44,7 @@ impl Eq for Item {}
 impl Controller {
     pub fn new(builder: &gtk::Builder) -> Self {
         Self {
-            items: Rc::new(RefCell::new(Vec::new())),
+            items: Rc::new(RefCell::new(HashMap::new())),
             list_box: get_widget!(builder, gtk::ListBox, "iridium-note-list"),
             title_entry: get_widget!(builder, gtk::Entry, "iridium-title-entry"),
             note_stack: get_widget!(builder, gtk::Stack, "right-hand-stack"),
@@ -77,21 +76,12 @@ impl Controller {
         // Do stupid insertion sort until we figured out how gtk::ListBox::set_sort_func's closure
         // could use the model itself.
         let mut items = self.items.borrow_mut();
-        let mut position = items.len();
 
-        for (i, item) in items.iter().enumerate() {
-            if item.last_updated < note.updated_at {
-                position = i;
-                break;
-            }
-        }
-
-        self.list_box.insert(&row, position as i32);
+        self.list_box.insert(&row, 0);
         self.list_box.select_row(Some(&row));
 
-        items.insert(position, Item {
+        items.insert(row, Item {
             uuid: note.uuid,
-            row: row.clone(),
             label: label.clone(),
             last_updated: note.updated_at,
         });
@@ -105,12 +95,12 @@ impl Controller {
         let mut index = 0;
         let mut items = self.items.borrow_mut();
 
-        for item in items.iter().filter(|item| item.uuid == *uuid) {
-            index = cmp::max(0, item.row.get_index() - 1);
-            self.list_box.remove(&item.row);
+        for (row, _) in items.iter().filter(|&(_, item)| item.uuid == *uuid) {
+            index = cmp::max(0, row.get_index() - 1);
+            self.list_box.remove(row);
         }
 
-        items.retain(|item| item.uuid != *uuid);
+        items.retain(|_, item| item.uuid != *uuid);
 
         if items.len() > 0 {
             let new_selected_row = self.list_box.get_row_at_index(index).unwrap();
@@ -128,8 +118,8 @@ impl Controller {
 
         let mut items = self.items.borrow_mut();
 
-        for item in items.iter() {
-            self.list_box.remove(&item.row);
+        for row in items.keys() {
+            self.list_box.remove(row);
         }
 
         items.clear();
@@ -137,8 +127,8 @@ impl Controller {
     }
 
     pub fn select_first(&self) {
-        if let Some(item) = self.items.borrow().get(0) {
-            self.list_box.select_row(Some(&item.row));
+        if let Some((row, _)) = self.items.borrow().iter().next() {
+            self.list_box.select_row(Some(row));
         }
     }
 
@@ -147,7 +137,7 @@ impl Controller {
             binding.unbind();
         }
 
-        for item in self.items.borrow().iter().filter(|item| item.row == *selected_row) {
+        if let Some(item) = self.items.borrow().get(selected_row) {
             self.binding = Some(self.title_entry.bind_property("text", &item.label, "label").build().unwrap());
             return Some(item.uuid);
         }
@@ -156,36 +146,36 @@ impl Controller {
     }
 
     pub fn updated(&mut self, uuid: &Uuid) {
-        for item in self.items.borrow_mut().iter_mut().filter(|item| item.uuid == *uuid) {
+        for (row, item) in self.items.borrow_mut().iter_mut().filter(|(_, item)| item.uuid == *uuid) {
             item.last_updated = Utc::now();
 
-            if item.row.get_index() > 0 {
-                self.list_box.remove(&item.row);
-                self.list_box.insert(&item.row, 0);
+            if row.get_index() > 0 {
+                self.list_box.remove(row);
+                self.list_box.insert(row, 0);
             }
         }
     }
 
     pub fn show_matching_rows(&self, term: &str) {
-        for item in self.items.borrow().iter() {
+        for (row, item) in self.items.borrow().iter() {
             let label_text = item.label.get_text().to_string().to_lowercase();
 
             if label_text.contains(&term) {
-                item.row.show();
+                row.show();
             }
             else {
-                item.row.hide();
+                row.hide();
             }
         }
     }
 
     pub fn show_all_rows(&self) {
-        for item in self.items.borrow().iter() {
-            item.row.show();
+        for row in self.items.borrow().keys() {
+            row.show();
         }
     }
 
     fn have(&self, uuid: &Uuid) -> bool {
-        self.items.borrow().iter().any(|item| item.uuid == *uuid)
+        self.items.borrow().iter().any(|(_, item)| item.uuid == *uuid)
     }
 }
