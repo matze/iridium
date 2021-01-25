d9797cd4b8b2b4fb8e5cfefec387a9c51ef76a05
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2021-01-25 21:27:19 +0100

    cli: add non-working signin command

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
-use standardfile::{Credentials, Exported};
 use standardfile::crypto::Crypto;
+use standardfile::remote::Client;
+use standardfile::{Credentials, Exported};
 use std::fs::read_to_string;
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
@@ -10,10 +11,17 @@ enum Command {
     Decrypt {
         #[structopt(long, parse(from_os_str))]
         input: PathBuf,
-
         #[structopt(long)]
         password: String,
-    }
+    },
+    Signin {
+        #[structopt(long)]
+        host: Option<String>,
+        #[structopt(long)]
+        identifier: String,
+        #[structopt(long)]
+        password: String,
+    },
 }
 
 fn decrypt(input: &Path, password: &str) -> Result<()> {
@@ -28,9 +36,25 @@ fn decrypt(input: &Path, password: &str) -> Result<()> {
     Ok(())
 }
 
+fn signin(host: Option<String>, identifier: &str, password: &str) -> Result<()> {
+    let credentials = Credentials::from_defaults(&identifier, &password);
+    let host = host.unwrap_or(String::from("https://sync.standardnotes.org"));
+    let _ = Client::new_sign_in(&host, &credentials)?;
+    Ok(())
+}
+
 fn main() -> Result<()> {
     match Command::from_args() {
-        Command::Decrypt {input, password} => { decrypt(&input, &password)?; }
+        Command::Decrypt { input, password } => {
+            decrypt(&input, &password)?;
+        }
+        Command::Signin {
+            host,
+            identifier,
+            password,
+        } => {
+            signin(host, &identifier, &password)?;
+        }
     };
 
     Ok(())
